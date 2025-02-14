8b033f111d96ee65f9629a3e91cbf9444885d9d0
Luka Guledani <lukaguledani123@gmail.com>
2025-02-14 17:45:47 +0400

    Database schema for user

---
+++ Back-end/schemas/user_schema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+// User schema for mongodb 
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required:true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        validate: {
+            validator: function (v) {
+                // regex to check if contains "@"
+                 return /^.+@.+\..+$/.test(v)
+            },
+            message: props => `${props.value} is not valid email! Email must contain "@"`
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const UserSchema= mongoose.model('UserSchema', userSchema)
+
+export default UserSchema
