b62369fe013deab53e9fb3969085be6f62958fa1
Luka Guledani <lukaguledani123@gmail.com>
2025-02-19 11:34:24 +0400

    Add: routes to access

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,9 +4,19 @@ import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
-import homeRoute from './routes/home_page_r.js'
-import signupRoute from './routes/signup_r.js'
-import loginRoute from './routes/login_r.js'
+// Routers
+import homeRouter from './routes/home_page_r.js'
+import signupRouter from './routes/signup_r.js'
+import loginRouter from './routes/login_r.js'
+import aboutUsRouter from './routes/about_us_r.js'
+import serviceRouter from './routes/service_r.js'
+import portfolioTeamRouter from './routes/portfolio_team_r.js'
+import blogRouter from './routes/blog_r.js'
+import getInTouchRouter from './routes/get_in_touch_r.js'
+import contactRouter from './routes/contact_r.js'
+import protectedRouter from './routes/protected_r.js'
+// Not working import forgetPassRouter from './routes/forget_password_r.js'
+
 
 // Environment variables
 dotenv.config()
@@ -24,13 +34,22 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.owba2.mongodb.net/`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tp89p.mongodb.net/`
 
 // Routes to access
-app.use('/', homeRoute)
-app.use('/signup', signupRoute)
-app.use('/login', loginRoute)
+app.use('/', homeRouter)
+app.use('/signup', signupRouter)
+app.use('/login', loginRouter)
+app.use('/aboutus', aboutUsRouter)
+app.use('/service', serviceRouter)
+app.use('/portfolioTeam', portfolioTeamRouter)
+app.use('/blog', blogRouter)
+app.use('/get-in-touch', getInTouchRouter)
+app.use('/contact', contactRouter)
+app.use('/protected', protectedRouter)
+// Not working app.use('/api/', forgetPassRouter)
 
+// Database connection
 mongoose
     .connect(url)
     .then(() => console.log("MongoDB connected"))
