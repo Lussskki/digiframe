3e521500868c81a3a2dd3f755f67795ff0365d4c
Luka Guledani <lukaguledani123@gmail.com>
2025-03-12 12:13:28 +0400

    Change: I separated the two POST methods and created individual routes.

--- Back-end/routes/forget_password_r.js
+++ Back-end/routes/forget_password_r.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import UserSchema from '../schemas/user_schema.js'
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 
@@ -20,7 +19,7 @@ const transporter = nodemailer.createTransport({
 })
 
 // Forgot Password Route
-forgetPassRouter.post('/forget-password', async (req, res) => {
+forgetPassRouter.post('/', async (req, res) => {
   const { email } = req.body
 
   try {
@@ -49,34 +48,6 @@ forgetPassRouter.post('/forget-password', async (req, res) => {
   } catch (error) {
     console.log('Error sending email:', error)
     res.status(500).json({ error: 'Error sending email' })
-  }
-})
-
-// Reset Password Route
-forgetPassRouter.post('/reset-password', async (req, res) => {
-  const { token, newPassword } = req.body
-
-  try {
-    // Verify JWT Token
-    const decoded = jwt.verify(token, process.env.SECRET)
-    const email = decoded.email
-
-    // Find User
-    const user = await UserSchema.findOne({ email })
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' })
-    }
-
-    // Hash New Password
-    const hashedPassword = await bcrypt.hash(newPassword, 10)
-
-    // Update Password
-    await UserSchema.updateOne({ email }, { password: hashedPassword })
-
-    res.status(200).json({ message: 'Password reset successfully' })
-  } catch (error) {
-    console.log('Error:', error)
-    res.status(400).json({ error: 'Invalid or expired token' })
   }
 })
 
