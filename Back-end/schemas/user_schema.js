fbbcf800248c8250f70f14a6cb76f7adfce6643c
Luka Guledani <lukaguledani123@gmail.com>
2025-03-12 12:08:39 +0400

    Remove: commented code

--- Back-end/schemas/user_schema.js
+++ Back-end/schemas/user_schema.js
@@ -25,13 +25,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    // Not working
-    // emailServicePassword: { // Field to reset a forgotten password
-    //   type: String,
-    //   select: false,
-    //   required: true
-    // }
+    }
 })
 
 const UserSchema= mongoose.model('UserSchema', userSchema)
